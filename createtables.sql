9767d80a09d001da5382e3f37f877c3c5f3861cc
hannahcat <kidwellhe@gmail.com>
2020-12-08 16:25:17 -0800

    initial exploration with Bokeh

--- createtables.sql
+++
@@ -1,7 +0,0 @@
-create table SpotifyGenre (category varchar(25), cattype varchar(25));
-
-insert into SpotifyGenre values ('acoustic','Genre'),('afrobeat','Genre'),('alt-rock','Genre');
-
-create table LyricsGenius (ArtistID int, SongID int, returndata varchar(500));
-
-insert into LyricsGenius values (502, null, null), (5566, 22, null);
